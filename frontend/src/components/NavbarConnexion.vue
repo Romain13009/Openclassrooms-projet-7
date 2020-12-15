68f27e4bdfa5a544f7f791cf3fbe2a313b920ce1
Romain VINAI <romain.vinai13009@gmail.com>
2020-12-15 16:26:25 +0100

    Navbar page connexion

---
+++ frontend/src/components/NavbarConnexion.vue
@@ -0,0 +1,29 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="collapse navbar-collapse justify-content-center">
+            <a href="#"><img class="w-50" src="../assets/icon-left-font-monochrome-white.svg" alt="Logo"/></a>
+        </div>
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">S'identifier</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-info" href="#">S'inscrire</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'NavbarConnexion',
+}
+</script>
+
+<style scoped>
+.navbar {
+    min-height: 80px;
+}
+</style>
