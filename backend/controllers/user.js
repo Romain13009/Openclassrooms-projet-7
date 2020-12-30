ad0d3d79cccbcff9b4cccd3b41f964e5cd74fddf
Romain VINAI <romain.vinai13009@gmail.com>
2020-12-30 18:41:54 +0100

    ajout de la fonction login et systeme d'auth par token, testée

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,6 +3,8 @@ const bcrypt = require('bcrypt'); //Notre système de cryptage
 const jwt = require('jsonwebtoken'); //permet de créer et de vérifier des tokens d'auth
 const models = require('../models');
 
+const JWT_CLE_SECRETE = '1azenh44e2r5v8b7n4h5t65dvvvtyu5i1f6cc7cn';
+
 //Controllers
 var regexPassword = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}/;
 var regexEmail = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z{2,8})(\.[a-z]{2,8})?$/;
@@ -36,8 +38,9 @@ exports.signup = (req, res, next) => {
                         });
                         newUser.save() //cette méthode enregistre dans la bdd
                             .then(() => res.status(201).json({ message: 'Utilisateur créé.'}))
-                            .catch(error => res.status(400).json({ error: 'Utilisateur ne peut pas etre créé' }));
+                            .catch(error => res.status(400).json({ error }));
                     })
+                    .catch(error => res.status(500).json({ error }));
                 } else {
                     return res.status(401).json({ error: 'Utilisateur déjà existant' });
                 }
@@ -46,5 +49,43 @@ exports.signup = (req, res, next) => {
     }
 };
 
+
+
 exports.login = (req, res, next) => {
+    //paramètres
+    var email = req.body.email;
+    var password = req.body.password;
+
+    if(email == null || password == null) {
+        return res.status(401).json({ error: 'Email, username ou password manquant' });
+    }
+
+    models.User.findOne({
+        where: { email: email}
+    })
+        .then(function(userFound) {
+            if (userFound) {
+                bcrypt.compare(password, userFound.password) //On utilise bcrypt.compare pour comparer le mdp envoyé dans le corps de la requête avec le hash enregistrer dans notre doc user
+                    .then(valid => {
+                        if (!valid) {
+                            return res.status(401).json({ error: 'Email ou password incorrect' });
+                        }
+                        res.status(200).json({ //si le mdp correspond on retourne un objet json
+                            userId: userFound.id,
+                            token: jwt.sign({ //fonction sign pour encoder
+                                userId: userFound.id,
+                                isAdmin: userFound.isAdmin},//1er argument: ce qque l'on veut encoder
+                                JWT_CLE_SECRETE, //2ème argument: clé secrète
+                                { expiresIn: '8h' } //3ème argument: argument de configuration
+                            )
+                        });             
+                    })
+                    .catch(function(){
+                        return res.status(401).json({ error });
+                    })
+            }else {
+                return res.status(401).json({ error: 'Email ou password incorrect' });
+            }
+        })
+        .catch(error => res.status(400).json({ error }));
 };
