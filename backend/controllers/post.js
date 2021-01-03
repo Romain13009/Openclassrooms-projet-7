a9f4153add86731ef995d6fee951c54431cc4108
Romain VINAI <romain.vinai13009@gmail.com>
2021-01-03 15:40:25 +0100

    ajout de la fonction de suppression de post selon les droits de l'utilisateur (tésté sans image, faudra tester avec)

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -61,6 +61,7 @@ exports.createPost = (req, res, next) => {
         })
 };
 
+
 //AFFICHER LES POSTS
 exports.showPost = (req, res, next) => {
     models.Post.findAll({
@@ -68,7 +69,8 @@ exports.showPost = (req, res, next) => {
         include: [{
             model: models.User,
             attributes: [ 'username']
-        }]
+        }],
+        limit: 10
     })
         .then(function(posts) {
             if (posts.length > null) {
@@ -79,5 +81,57 @@ exports.showPost = (req, res, next) => {
         })
         .catch(function(){
             return res.status(404).json({ error: 'error' });
+        })
+}
+
+
+//SUPPRIMER UN POST
+exports.deletePost = (req, res, next) => {
+    //Paramètres
+    const token = req.headers.authorization.split(' ')[1]; //On récupère le token (on split autour de l'espace), on récupère un tableau dont on prend le second élément (le 1)
+    const decodedToken = jwt.verify(token, JWT_CLE_SECRETE); //On décode le token, on utilise la clé secrete, le token décodé devient un objet js
+    const userId = decodedToken.userId; //On récupère l'id de la réponse
+    const idPost = req.body.id
+    
+
+    models.Post.findOne({
+        attributes: ['id', 'content', 'UserId'],
+        where: { id: idPost}
+    })
+        .then(function(postFound) {
+            models.User.findOne({
+                attributes: ['id', 'isAdmin'],
+                where: { id: userId }
+            })
+                .then(user => {
+                    if ( user.isAdmin == true || user.id == postFound.UserId ) {
+
+                        if (postFound.image) {
+                            const filename = postFound.image.split('/images/')[1]
+                            fs.unlink(`images/${filename}`, () => {
+                                models.Post.destroy({ 
+                                    where: { id: idPost}
+                                })
+                                    .then(() => res.status(200).json({ message: 'Publication et image supprimées' }))
+                                    .catch(error => res.status(400).json({ error }));
+                            });
+                        } else {
+                            
+                            models.Post.destroy({
+                            where: { id: idPost }
+                            })
+                                .then(() => res.status(200).json({ message: 'Publication supprimée' }))
+                                .catch(error => res.status(400).json({ error }));
+                        } 
+                    } else {
+                        return res.status(500).json({ error: 'action non autorisée' });
+                    }
+                })
+                .catch(function(){
+                    return res.status(404).json({ error: 'Utilisateur introuvable ou action non autorisée' });
+                })
+        }) 
+        .catch(function(){
+            return res.status(404).json({ error: 'Publication introuvable' });
         })
 }
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,12 +1,13 @@
 //routes de la partie post
 const express = require('express');
 const router = express.Router();
-const userCtrl = require('../controllers/post');
+const postCtrl = require('../controllers/post');
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
 //Routes
-router.post('/new/', auth, multer, userCtrl.createPost);
-router.get('/', auth, multer,userCtrl.showPost);
+router.post('/new/', auth, multer, postCtrl.createPost);
+router.get('/', auth, multer, postCtrl.showPost);
+router.delete('/delete', auth, postCtrl.deletePost);
 
 module.exports = router;
